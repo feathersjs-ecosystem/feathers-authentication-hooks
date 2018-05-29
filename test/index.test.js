023b7901ef3b6c011b15d739148d8a2c4449c197
David Luecke <daff@neyeon.com>
2018-05-29 14:55:46 -0700

    Remove role related hooks (#54)

--- test/index.test.js
+++ test/index.test.js
@@ -22,15 +22,7 @@ describe('Auth hooks', () => {
     expect(typeof hooks.restrictToAuthenticated).to.equal('function');
   });
 
-  it('exposes restrictToRoles hook', () => {
-    expect(typeof hooks.restrictToRoles).to.equal('function');
-  });
-
   it('exposes restrictToOwner hook', () => {
     expect(typeof hooks.restrictToOwner).to.equal('function');
-  });
-
-  it('exposes hasRoleOrRestrict hook', () => {
-    expect(typeof hooks.hasRoleOrRestrict).to.equal('function');
   });
 });
