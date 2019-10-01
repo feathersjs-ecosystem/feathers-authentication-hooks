25fd0cadefc41f213211144de2f3c3da7e740bfe
David Luecke <daff@neyeon.com>
2019-09-30 22:18:22 -0700

    Refactor into single setField hook for Feathers 4

--- test/index.test.js
+++ test/index.test.js
@@ -1,24 +1,8 @@
-const { expect } = require('chai');
+const assert = require('assert');
 const hooks = require('../lib/index');
 
-describe('Auth hooks', () => {
-  it('is CommonJS compatible', () => {
-    expect(typeof require('../lib/index')).to.equal('object');
-  });
-
-  it('is ES6 compatible', () => {
-    expect(typeof hooks).to.equal('object');
-  });
-
-  it('exposes associateCurrentUser hook', () => {
-    expect(typeof hooks.associateCurrentUser).to.equal('function');
-  });
-
-  it('exposes queryWithCurrentUser hook', () => {
-    expect(typeof hooks.queryWithCurrentUser).to.equal('function');
-  });
-
-  it('exposes restrictToOwner hook', () => {
-    expect(typeof hooks.restrictToOwner).to.equal('function');
+describe('feathers-authentication-hooks', () => {
+  it('exposes setField', () => {
+    assert.ok(typeof hooks.setField === 'function');
   });
 });
